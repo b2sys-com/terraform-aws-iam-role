10a8ac478f71bb1107606997ebc8bccae01c5a19
diegosorace <dsorace@prismamp.com>
2023-02-26 02:37:15 -0300

    update

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "id" {
   description = "The role's ARN"
-  value       = try(aws_iam_role.achubRole.id, "")
+  value       = try(aws_iam_role.this.id, "")
 }
 
 output "arn" {
   description = "The role's ARN"
-  value       = try(aws_iam_role.achubRole.arn, "")
+  value       = try(aws_iam_role.this.arn, "")
 }
